6da2140d389cbfd6b66cc1aa98b349258008b920
Zoltan Jakubecz <jakuzo@gmail.com>
2021-01-17 20:08:42 +0100

    remove unused import

--- src/main/java/com/jakuza/statemachine/config/StateMachineConfiguration.java
+++ src/main/java/com/jakuza/statemachine/config/StateMachineConfiguration.java
@@ -3,7 +3,6 @@ package com.jakuza.statemachine.config;
 import com.jakuza.statemachine.action.ApplicationEvent;
 import com.jakuza.statemachine.action.ApplicationState;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
